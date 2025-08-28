ac98f36477e6aae17b161da8d0e9778a433af140
auto-code-etri <sheart@etri.re.kr>
2025-08-28 10:39:39 +0900

    GithubCrawling directory was removed

--- GithubCrawling/github_crawling/Codes/0016/0016_c/hello.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int				main(void)
-{
-					write(1, "Hello World!\n", 13);
-					return(0);
-}
-
--- GithubCrawling/github_crawling/Codes/0016/0016_c/maff_alpha.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int				main(void)
-{
-					write (1, "aBcDeFgHiJkLmNoPqRsTuVwXyZ\n", 27);
-					return(0);
-}
-
